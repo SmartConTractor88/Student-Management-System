f7e1559c272a9b91cf0b01990c1c9107e7e774ef
SmartConTractor <login.blockchain888@gmail.com>
2025-03-23 11:35:03 +0100

    Connect to the database. Load data into a table.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, \
-    QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget
+    QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, \
+    QTableWidgetItem
 
 from PyQt6.QtGui import QAction
 
@@ -26,20 +27,34 @@ class MainWindow(QMainWindow):
 
         self.table = QTableWidget()
         self.table.setColumnCount(4)
-        self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Contact"))
+        self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", 
+                                              "Contact"))
+        self.table.verticalHeader().setVisible(False) # hide the first index column 
         self.setCentralWidget(self.table) # specify the central widget
 
     def load_data(self):
-        self.table
-        pass
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+
+        """#whenever the program is loaded, the following data will not be 
+        added on top of existing data"""
+        self.table.setRowCount(0)
+
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, 
+                                   QTableWidgetItem(str(data)))
+        connection.close()
 
         #grid = QGridLayout()
-
         #self.setLayout(grid)
 
+
 app = QApplication(sys.argv)
-just_something = MainWindow()
-just_something.show()
+run_app = MainWindow() # create main window
+run_app.load_data()
+run_app.show()
 sys.exit(app.exec())
 
 #class_init = MainWindow()
